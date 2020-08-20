1b1196e58c27e10cc57198c94c46d2d0f2208d6f
JGworek <54279989+JGworek@users.noreply.github.com>
2020-08-20 14:37:26 -0400

    Update environment.prod.ts

--- fothu-zone/src/environments/environment.prod.ts
+++ fothu-zone/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+const ERROR_STATUS_CODES: Array<Number> = [400, 401, 402, 403, 404, 405, 500, 501, 502, 503, 504, 505];
+
 export const environment = {
-  production: true
+  production: true,
+  errorCodes: ERROR_STATUS_CODES,
 };
