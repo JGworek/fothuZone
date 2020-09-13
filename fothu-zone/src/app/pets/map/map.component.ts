f4051235d5b2b6c5365d36e71e460d978f360f96
unknown <moberlies1337@gmail.com>
2020-09-13 00:23:55 -0700

    Further refactor to simplify Pets Map Component

--- fothu-zone/src/app/pets/map/map.component.ts
+++ fothu-zone/src/app/pets/map/map.component.ts
@@ -9,7 +9,6 @@ export class MapComponent implements OnInit {
 
   constructor() { }
 
-  test: Array<number> = new Array(10);
   truthArray: Array<boolean> = new Array<boolean>(100);
   notExploredArray: Array<boolean> = new Array<boolean>(100);
   nonBarrenCells: Array<number> = [11, 12, 13, 21, 23, 25, 26, 27, 30, 31, 32, 33, 35, 37, 38, 41, 43, 44, 45, 47, 51, 52, 57, 65, 66, 67, 73, 74, 75, 85, 86, 87, 88, 89];
@@ -28,8 +27,6 @@ export class MapComponent implements OnInit {
 
   onSameRow(roomId: number) {
     // Check that the tens digit is the same
-    // console.log("On same row for current cell: ", Math.floor(this.currentRoom / 10));
-    // console.log("On same row for target cell #", roomId, ": ", Math.floor(roomId / 10));
     return Math.floor(this.currentRoom / 10) === Math.floor(roomId / 10);
   }
 
@@ -44,9 +41,6 @@ export class MapComponent implements OnInit {
   }
 
   setCurrentRoom(roomId: number) {
-    // console.log(roomId);
-    console.log(this.notExploredArray.length);
-
     this.truthArray[this.currentRoom] = false;
     this.currentRoom = roomId;
 
