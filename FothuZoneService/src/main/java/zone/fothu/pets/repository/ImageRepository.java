b5d850e1424c5cd58ee89c139eb82e726ca00623
JGworek <54279989+JGworek@users.noreply.github.com>
2020-12-09 20:13:14 -0500

    Update ImageRepository.java

--- FothuZoneService/src/main/java/zone/fothu/pets/repository/ImageRepository.java
+++ FothuZoneService/src/main/java/zone/fothu/pets/repository/ImageRepository.java
@@ -11,8 +11,8 @@ public interface ImageRepository extends JpaRepository<Image, Integer> {
     
     @Modifying
     @Transactional
-    @Query(nativeQuery = true, value = "INSERT INTO pets.images VALUES (DEFAULT, ?1, ?2, ?3)")
-    void saveNewImage(String imageURL, String imageOwnerUsername, String imageOwnerName);
+    @Query(nativeQuery = true, value = "INSERT INTO pets.images VALUES (DEFAULT, ?1)")
+    void saveNewImage(String imageURL);
     
     @Query(nativeQuery = true, value = "SELECT MAX(id) FROM pets.images")
     int findLatestImageId();
