40e4da7258668d5bed964374fe3d0c8f1b8be4ab
JGworek <54279989+JGworek@users.noreply.github.com>
2021-11-01 20:11:36 -0400

    Update environment.prod.ts

--- fothu-zone/src/environments/environment.prod.ts
+++ fothu-zone/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ const ERROR_STATUS_CODES: Array<Number> = [400, 401, 402, 403, 404, 405, 500, 50
 export const environment = {
   production: true,
   errorCodes: ERROR_STATUS_CODES,
-  fothuZoneEC2Link: "http://ec2-3-95-255-63.compute-1.amazonaws.com:6969",
+  fothuZoneEC2Link: "http://ec2-3-89-74-168.compute-1.amazonaws.com:6969",
 };
